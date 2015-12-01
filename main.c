eec04c2276cda24dd0455ec8cde889196fedb4a9
Seif HABBACHI <shabbach@e3r3p12.42.fr>
2015-12-01 18:25:43 +0100

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 14:57:14 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/01 17:00:39 by tiprata          ###   ########.fr       */
+/*   Updated: 2015/12/01 17:59:49 by shabbach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ int     main(int ac, char **av)
 {
 	t_file file;
 
-	if (ac > 1)
+	if (ac != 2)
 	{
-		file = ft_fillit_read_open(av);
-		if (ft_error_check(file) == -1)
-		{
-			write(1, "error\n", 6);
-			return (0);
-		}
+		write(1, "error\n", 6);
+		return (0);
 	}
+
+	file = ft_fillit_read_open(av);
+	if (ft_error_check(file) == -1)
+	{
+		write(1, "error\n", 6);
+		return (0);
+	}
+	write (1, file.total[0], ft_strlen(file.total[0]));
     return (0);
 }
