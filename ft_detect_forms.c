127723f177403022872eca39db62df97f122ea97
Timon PRATA <tiprata@e1r2p19.42.fr>
2015-12-02 20:53:22 +0100

    detect

--- ft_detect_forms.c
+++ ft_detect_forms.c
@@ -6,106 +6,12 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 19:07:16 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/02 20:32:19 by tiprata          ###   ########.fr       */
+/*   Updated: 2015/12/02 20:53:14 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_form	ft_is_square(t_file file, t_form form, t_pos pos)
-{
-	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
-		{
-			if (file.total[pos.x][pos.y] == '\n')
-				pos.x++;
-			pos.y++;
-		}
-	if (file.total[pos.x][pos.y + 1] == '#')
-		if (file.total[pos.x + 1][pos.y] == '#')
-			if (file.total[pos.x + 1][pos.y + 1])
-				form.square++;
-	return (form);
-}
-
-static t_form	ft_is_stick(t_file file, t_form form, t_pos pos, int i)
-{
-	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
-		{
-			if (file.total[pos.x][pos.y] == '\n')
-				pos.x++;
-			pos.y++;
-		}
-	if (file.total[pos.x][pos.y + 1] == '#')
-	{
-		while (file.total[pos.x][pos.y] == '#')
-			pos.y++;
-		if (pos.y == 3 * i)
-			form.sd++;
-	}
-		if (file.total[pos.x + 1][pos.y] == '#')
-		{
-			while (file.total[pos.x][pos.y] == '#')
-				pos.x++;
-			if (pos.x == 3 * i)
-				form.su++;
-		}
-	return (form)
-}
-
-static t_form	ft_is_t(t_file file, t_form form, t_pos pos)
-{
-	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
-		{
-			if (file.total[pos.x][pos.y] == '\n')
-				pos.x++;
-			pos.y++;
-		}
-	if (file.total[pos.x + 1][pos.y] == '#' &&
-		file.total[pos.x + 2][pos.y] == '#')
-	{
-		if (file.total[pos.x + 1][pos.y - 1] == '#')
-			form.lt++;
-		if (file.total[pos.x + 1][pos.y + 1] == '#')
-			form.rit++;
-	}
-	if (file.total[pos.x][pos.y + 1] == '#' &&
-		file.total[pos.x][pos.y + 2] == '#')
-		if (file.total[pos.x + 1][pos.y + 1] == '#')
-			form.t++;
-	if (file.total[pos.x + 1][pos.y - 1] == '#' &&
-		file.total[pos.x + 1][pos.y] == '#')
-		if (file.total[pos.x + 1][pos.y + 1] == '#')
-			form.rt++;
-	return (form)
-}
-
-static t_form	ft_is_sharp(t_file file, t_form form, t_pos pos)
-{
-	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
-		{
-			if (file.total[pos.x][pos.y] == '\n')
-				pos.x++;
-			pos.y++;
-		}
-	if (file.total[pos.x][pos.y + 1] == '#')
-	{
-		if (file.total[pos.x + 1][pos.y] == '#' &&
-			file.total[pos.x + 1][pos.y + 1] == '#')
-			form.rsh++;
-		if (file.total[pos.x + 1][pos.y] == '#' &&
-			file.total[pos.x + 1][pos.y - 1] == '#')
-			form.sh++;
-	}
-	if (file.total[pos.x + 1][pos.y] == '#')
-	{
-		if (file.total[pos.x + 1][pos.y + 1] == '#' &&
-			file.total[pos.x + 2][pos.y + 1] == '#')
-			form.ush++;
-		if (file.total[pos.x + 1][pos.y - 1] == '#' &&
-			file.total[pos.x + 2][pos.y - 1] == '#')
-			form.ursh++;
-	}
-}
 t_form	ft_detect_forms(t_file file)
 {
 	t_form form;
