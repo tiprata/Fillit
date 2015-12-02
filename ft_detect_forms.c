a935a8887baac17489e11df831b878a05f3e7b99
Timon PRATA <tiprata@e1r2p17.42.fr>
2015-12-02 14:41:22 +0100

    detect

--- ft_detect_forms.c
+++ ft_detect_forms.c
@@ -6,24 +6,65 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 19:07:16 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/01 19:21:51 by tiprata          ###   ########.fr       */
+/*   Updated: 2015/12/02 14:41:13 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_form	ft_is_square(char **str)
+static t_form	ft_is_square(t_file file, t_form form, t_pos pos)
 {
-	if (
+	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
+		{
+			if (file.total[pos.x][pos.y] == '\n')
+				pos.x++;
+			pos.y++;
+		}
+	if (file.total[pos.x][pos.y + 1] == '#')
+		if (file.total[pos.x + 1][pos.y] == '#')
+			if (file.total[pos.x + 1][pos.y + 1])
+				form.square++;
+	return (form);
+}
+
+static t_form	ft_is_stick(t_file file, t_form form, t_pos pos, int i)
+{
+	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
+		{
+			if (file.total[pos.x][pos.y] == '\n')
+				pos.x++;
+			pos.y++;
+		}
+	if (file.total[pos.x][pos.y + 1] == '#')
+	{
+		while (file.total[pos.x][pos.y] == '#')
+			pos.y++;
+		if (pos.y == 3 * i)
+			form.sd++;
+	}
+		if (file.total[pos.x + 1][pos.y] == '#')
+		{
+			while (file.total[pos.x][pos.y] == '#')
+				pos.x++;
+			if (pos.x == 3 * i)
+				form.su++; 
+		}
+	return (form)
 }
 
 t_form	ft_detect_forms(t_file file)
 {
 	t_form form;
+	t_pos pos;
 	int i;
+
+	pos.x = 0;
+	pos.y = 0;
 	while (i < file.tetrinb)
 	{
-		t_form = ft_is_square(file.total);
+		pos.x = i * 5;
+		pos.y = 0;
+		t_form = ft_is_square(file, form, pos, i);
 		i++;
 	}
 	return (form);
