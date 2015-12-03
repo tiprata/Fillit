c1dfc1fdef38f0e82acf33a0480e8555dcd1914b
Timon PRATA <tiprata@e1r2p19.42.fr>
2015-12-03 03:05:48 +0100

    iaulau

--- ft_is_stick.c
+++ ft_is_stick.c
@@ -6,33 +6,22 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 20:41:20 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/02 20:43:10 by tiprata          ###   ########.fr       */
+/*   Updated: 2015/12/03 03:05:16 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_form   ft_is_stick(t_file file, t_form form, t_pos pos, int i)
+t_form   ft_is_stick(char *str, t_form form)
 {
-	while (file.total[pos.x][pos.y] != '#' && file.total[pos.x][pos.y])
-	{
-		if (file.total[pos.x][pos.y] == '\n')
-			pos.x++;
-		pos.y++;
-	}
-	if (file.total[pos.x][pos.y + 1] == '#')
-	{
-		while (file.total[pos.x][pos.y] == '#')
-			pos.y++;
-		if (pos.y == 3 * i)
-			form.sd++;
-	}
-	if (file.total[pos.x + 1][pos.y] == '#')
-	{
-		while (file.total[pos.x][pos.y] == '#')
-			pos.x++;
-		if (pos.x == 3 * i)
-			form.su++;
-	}
+	int i;
+
+	i = 0;
+	while (str[i] && str[i] != '#')
+		i++;
+	if (str[i + 1] == '#' && str[i + 2] == '#' && str[i + 3] == '#')
+		form.su++;
+	if (str[i + 4] == '#' && str[i + 8] == '#' && str[i + 12] == '#')
+		form.sd++;
     return (form);
 }
