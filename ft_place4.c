341301b338fbe0335fcaaa72d2a62324f6539d70
Timon PRATA <tiprata@e1r11p23.42.fr>
2015-12-08 13:22:44 +0100

    ok

--- ft_place4.c
+++ ft_place4.c
@@ -6,7 +6,7 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 18:42:10 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/07 18:55:29 by tiprata          ###   ########.fr       */
+/*   Updated: 2015/12/08 13:20:51 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,29 +44,29 @@ char	**ft_s(char *str, char **str, int k)
 	return (str);
 }
 
-char	**ft_ill(char *str, char **str, int k)
+char	**ft_z(char *str, char **str, int k)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 1;
-	while (i < (k * 3) - 3 && j < (k * 3) - 2)
+	while (i < (k * 3) - 2 && j < (k * 3) - 3)
 	{
-		if (str[i][j] == '.' && str[i + 1][j] == '.'
-			&& str[i + 2][j] == '.' && str[i + 2][j - 1] == '.')
+		if (str[i][j] == '.' && str[i][j + 1] == '.'
+			&& str[i + 1][j + 1] == '.' && str[i + 1][j + 2] == '.')
 		{
 			str[i][j] = '#';
-			str[i + 1][j] = '#';
-			str[i + 2][j] = '#';
-			str[i + 2][j - 1] = '#';
+			str[i][j + 1] = '#';
+			str[i + 1][j + 1] = '#';
+			str[i + 1][j + 2] = '#';
 			return (str);
 		}
 		else
 		{
 			j++;
 		}
-		if (j = (k * 3) - 3)
+		if (j = (k * 3) - 4)
 		{
 			i++;
 			j = 1;
@@ -75,53 +75,53 @@ char	**ft_ill(char *str, char **str, int k)
 	return (str);
 }
 
-char	**ft_isl(char *str, char **str, int k)
+char	**ft_ups(char *str, char **str, int k)
 {
 	int i;
 	int j;
 
 	i = 0;
-	j = 0;
-	while (i < (k * 3) - 4 && j < (k * 3) - 1)
+	j = 1;
+	while (i < (k * 3) - 3 && j < (k * 3) - 2)
 	{
-		if (str[i][j] == '.' && str[i + 1][j] == '.' && str[i + 2][j] == '.' &&
-			str[i + 2][j + 1] == '.')
+		if (str[i][j] == '.' && str[i + 1][j - 1] == '.' && str[i + 1][j] == '.' &&
+			str[i + 2][j - 1] == '.')
 		{
 			str[i][j] = '#';
+			str[i + 1][j - 1] = '#';
 			str[i + 1][j] = '#';
-			str[i + 2][j] = '#';
-			str[i + 2][j + 1] = '#';
+			str[i + 2][j - 1] = '#';
 			return (str);
 		}
 		else
 		{
 			j++;
 		}
-		if (k == (k * 3) - 2)
+		if (k == (k * 3) - 3)
 		{
 			i++;
-			j = 0;
+			j = 1;
 		}
 	}
 	return (str);
 }
 
-char	**ft_irsl(char *str, char **str, int k)
+char	**ft_upz(char *str, char **str, int k)
 {
 	int i;
 	int j;
 
 	i = 0;
-	j = 0;
-	while (i < (k * 3) - 2 && j < (k * 3) - 3)
+	j = 1;
+	while (i < (k * 3) - 3 && j < (k * 3) - 2)
 	{
-		if (str[i][j] == '.' && str[i][j + 1] == '.' && str[i][j + 2] == '.' &&
-			str[i + 1][j + 2] == '.')
+		if (str[i][j] == '.' && str[i + 1][j] == '.' && str[i + 1][j + 1] == '.' &&
+			str[i + 2][j + 1] == '.')
 		{
 			str[i][j] = '#';
-			str[i][j + 1] = '#';
-			str[i][j + 2] = '#';
-			str[i + 1][j + 2] = '#';
+			str[i + 1][j] = '#';
+			str[i + 1][j + 1] = '#';
+			str[i + 2][j + 1] = '#';
 			return (str);
 		}
 		else
@@ -131,7 +131,7 @@ char	**ft_irsl(char *str, char **str, int k)
 		if (j == (k * 3) - 3)
 		{
 			i++;
-			j = 0;
+			j = 1;
 		}
 	}
 	return (str);
